af39cf2c24f4a0bb1f511d3b536f78e77896f574
Rajveer Singh <117706744+rajveersinghcse@users.noreply.github.com>
2022-12-01 09:39:55 +0530

    new

--- text.py
+++ text.py
@@ -1,10 +1,10 @@
 from gtts import gTTS  #important libraries for this program
-import pytesseract
+import pytesseract  #to install this library you need to run this command ---> pip install pytesseract
 import os
 from PIL import Image
 import cv2
 
-image=cv2.imread("download.png")  #read the image
+image=cv2.imread("download.png")  #read the image it a sample image 
 cv2.imshow('input image',image)
 print(image.shape)
 print(image.size)
@@ -20,7 +20,7 @@ ret,bw=cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY)
 cv2.imshow('black and white',bw)
 
 #image to text
-pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
+pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe" #i use tesseract 
 result=pytesseract.image_to_string(image)
 print(result)
 
