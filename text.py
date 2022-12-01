41a67aa031c591d60a6b3dd0b623b1eb40758615
Rajveer Singh <117706744+rajveersinghcse@users.noreply.github.com>
2022-12-01 08:49:29 +0530

    new

--- text.py
+++ text.py
@@ -4,24 +4,24 @@ import os
 from PIL import Image
 import cv2
 
-img=cv2.imread("download.png")
-cv2.imshow('input image',img)
-print(img.shape)
-print(img.size)
-print(type(img))
+image=cv2.imread("download.png")
+cv2.imshow('input image',image)
+print(image.shape)
+print(image.size)
+print(type(image))
 
-img = cv2.resize(img,(200,250))
-cv2.imshow('resized image',img) #to show the image in text
-cv2.imwrite('img.png',img)
-cv2.imshow('new',img)
-gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+image = cv2.resize(image,(200,250))
+cv2.imshow('resized image',image) #to show the image in text
+cv2.imwrite('image.png',image)
+cv2.imshow('new',image)
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 cv2.imshow('gray',gray)
 ret,bw=cv2.threshold(gray, 100, 255, cv2.THRESH_BINARY)
 cv2.imshow('black and white',bw)
 
 #image to text
 pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tesseract.exe"
-result=pytesseract.image_to_string(img)
+result=pytesseract.image_to_string(image)
 print(result)
 
 ##text to speech
